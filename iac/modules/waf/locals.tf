b99bf5ef6756f861c025e494c6772542f0442cf7
jeancdev <jcode2006@gmail.com>
2026-06-18 17:31:15 -0500

    feat(iac): define managed rule groups and priorities for WAF in locals configuration

--- iac/modules/waf/locals.tf
+++ iac/modules/waf/locals.tf
@@ -1,3 +1,32 @@
 locals {
   name_prefix = "${var.project_name}-${var.environment}"
+
+  managed_rule_groups = [
+    "AWSManagedRulesAmazonIpReputationList",
+    "AWSManagedRulesAnonymousIpList",
+    "AWSManagedRulesKnownBadInputsRuleSet",
+    "AWSManagedRulesCommonRuleSet",
+    "AWSManagedRulesSQLiRuleSet",
+  ]
+
+  api_managed_rule_priorities = {
+    AWSManagedRulesAmazonIpReputationList = 10
+    AWSManagedRulesAnonymousIpList        = 20
+    AWSManagedRulesKnownBadInputsRuleSet  = 30
+    AWSManagedRulesCommonRuleSet          = 40
+    AWSManagedRulesSQLiRuleSet            = 50
+  }
+
+  cloudfront_managed_rule_priorities = {
+    AWSManagedRulesAmazonIpReputationList = 10
+    AWSManagedRulesAnonymousIpList        = 20
+    AWSManagedRulesKnownBadInputsRuleSet  = 30
+    AWSManagedRulesCommonRuleSet          = 40
+    AWSManagedRulesSQLiRuleSet            = 50
+  }
+
+  api_geo_rule_priority        = 60
+  cloudfront_geo_rule_priority = 60
+  api_bot_rule_priority        = 70
+  cloudfront_bot_rule_priority = 70
 }
