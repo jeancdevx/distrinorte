501e11b876335168b485a568591fca916ebd7cb3
jeancdev <jcode2006@gmail.com>
2026-06-18 17:31:04 -0500

    feat(iac): enhance WAF rules for CloudFront with dynamic rule configurations and geo-restriction support

--- iac/modules/waf/cloudfront-web-acl.tf
+++ iac/modules/waf/cloudfront-web-acl.tf
@@ -29,25 +29,87 @@ resource "aws_wafv2_web_acl" "cloudfront" {
     }
   }
 
-  rule {
-    name     = "AWSManagedRulesCommonRuleSet"
-    priority = 2
+  dynamic "rule" {
+    for_each = local.managed_rule_groups
+    content {
+      name     = rule.value
+      priority = local.cloudfront_managed_rule_priorities[rule.value]
 
-    override_action {
-      none {}
-    }
+      override_action {
+        none {}
+      }
 
-    statement {
-      managed_rule_group_statement {
-        name        = "AWSManagedRulesCommonRuleSet"
-        vendor_name = "AWS"
+      statement {
+        managed_rule_group_statement {
+          name        = rule.value
+          vendor_name = "AWS"
+        }
+      }
+
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = rule.value
+        sampled_requests_enabled   = true
       }
     }
+  }
 
-    visibility_config {
-      cloudwatch_metrics_enabled = true
-      metric_name                = "AWSManagedRulesCommonRuleSet"
-      sampled_requests_enabled   = true
+  dynamic "rule" {
+    for_each = var.enable_geo_restriction ? [1] : []
+    content {
+      name     = "GeoAllowList"
+      priority = local.cloudfront_geo_rule_priority
+
+      action {
+        block {}
+      }
+
+      statement {
+        not_statement {
+          statement {
+            geo_match_statement {
+              country_codes = var.allowed_country_codes
+            }
+          }
+        }
+      }
+
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = "GeoAllowList"
+        sampled_requests_enabled   = true
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = var.enable_bot_control ? [1] : []
+    content {
+      name     = "AWSManagedRulesBotControlRuleSet"
+      priority = local.cloudfront_bot_rule_priority
+
+      override_action {
+        none {}
+      }
+
+      statement {
+        managed_rule_group_statement {
+          name        = "AWSManagedRulesBotControlRuleSet"
+          vendor_name = "AWS"
+
+          managed_rule_group_configs {
+            aws_managed_rules_bot_control_rule_set {
+              inspection_level = var.bot_control_inspection_level
+            }
+          }
+        }
+      }
+
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = "AWSManagedRulesBotControlRuleSet"
+        sampled_requests_enabled   = true
+      }
     }
   }
 
