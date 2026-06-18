04d326f1a254026c7b21a0504c7ccace2256fd09
jeancdev <jcode2006@gmail.com>
2026-06-18 16:38:26 -0500

    feat(iac): add AWS API Gateway authorizer for Cognito user pools

---
+++ iac/modules/apigateway/authorizers.tf
@@ -0,0 +1,8 @@
+resource "aws_api_gateway_authorizer" "cognito" {
+  name          = "${local.name_prefix}-cognito"
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  type          = "COGNITO_USER_POOLS"
+  provider_arns = [var.cognito_user_pool_arn]
+
+  identity_source = "method.request.header.Authorization"
+}
