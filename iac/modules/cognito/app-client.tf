86b8e4245408647c11db790e81f0f33863d391a5
jeancdev <jcode2006@gmail.com>
2026-06-18 15:52:27 -0500

    feat(iac): add Cognito user pool client configuration for SPA

---
+++ iac/modules/cognito/app-client.tf
@@ -0,0 +1,42 @@
+resource "aws_cognito_user_pool_client" "spa" {
+  name         = "${local.name_prefix}-spa-client"
+  user_pool_id = aws_cognito_user_pool.main.id
+
+  generate_secret = false
+
+  explicit_auth_flows = [
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH",
+  ]
+
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_scopes                 = ["openid", "email", "profile"]
+
+  callback_urls = var.callback_urls
+  logout_urls   = var.logout_urls
+
+  supported_identity_providers = ["COGNITO"]
+
+  prevent_user_existence_errors = "ENABLED"
+
+  access_token_validity  = var.access_token_validity_hours
+  id_token_validity      = var.id_token_validity_hours
+  refresh_token_validity = var.refresh_token_validity_days
+
+  token_validity_units {
+    access_token  = "hours"
+    id_token      = "hours"
+    refresh_token = "days"
+  }
+
+  read_attributes = [
+    "email",
+    "custom:customer_id",
+  ]
+
+  write_attributes = [
+    "email",
+    "custom:customer_id",
+  ]
+}
