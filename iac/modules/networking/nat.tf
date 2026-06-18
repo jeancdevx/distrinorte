d99b1f1ffa41b8d8ae76c723da83c90679ac3792
jeancdev <jcode2006@gmail.com>
2026-06-18 04:15:50 -0500

    feat(iac): add NAT gateway and EIP resources to networking module

---
+++ iac/modules/networking/nat.tf
@@ -0,0 +1,23 @@
+resource "aws_eip" "nat" {
+  count  = local.nat_count
+  domain = "vpc"
+
+  tags = merge(var.tags, {
+    Name = "${local.name_prefix}-nat-eip-${count.index + 1}"
+  })
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_nat_gateway" "main" {
+  count = local.nat_count
+
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[local.nat_azs[count.index]].id
+
+  tags = merge(var.tags, {
+    Name = "${local.name_prefix}-nat-${count.index + 1}"
+  })
+
+  depends_on = [aws_internet_gateway.main]
+}
