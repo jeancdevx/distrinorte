c1f4af52cbacc3e74ebd8849fb2cfbe0c5c647e7
jeancdev <jcode2006@gmail.com>
2026-06-18 15:15:21 -0500

    feat(iac): add ECS cluster capacity providers configuration

---
+++ iac/modules/ecs-cluster/capacity-providers.tf
@@ -0,0 +1,11 @@
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name = aws_ecs_cluster.main.name
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
+  }
+}
