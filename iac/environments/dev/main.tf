c3d183c751d63a5f7bb34a68ab7324d2de7c79e7
jeancdev <jcode2006@gmail.com>
2026-06-18 17:07:07 -0500

    refactor(iac): reorganize CloudFront module configuration and integrate WAF settings

--- iac/environments/dev/main.tf
+++ iac/environments/dev/main.tf
@@ -329,6 +329,23 @@ module "apigateway" {
   stage_name                  = local.environment
 }
 
+module "waf" {
+  source = "../../modules/waf"
+
+  project_name = local.project_name
+  environment  = local.environment
+  tags         = local.common_tags
+
+  origin_verify_secret  = var.origin_verify_secret
+  api_gateway_stage_arn = module.apigateway.stage_arn
+  cloudfront_rate_limit = var.waf_cloudfront_rate_limit
+
+  providers = {
+    aws           = aws
+    aws.us_east_1 = aws.us_east_1
+  }
+}
+
 module "cloudfront" {
   source = "../../modules/cloudfront"
 
@@ -345,41 +362,12 @@ module "cloudfront" {
   api_gateway_rest_api_id = module.apigateway.rest_api_id
   api_gateway_stage_name  = module.apigateway.stage_name
   origin_verify_secret    = var.origin_verify_secret
+  web_acl_id              = module.waf.cloudfront_web_acl_arn
 
-  portal_aliases      = var.cloudfront_portal_aliases
-  api_aliases         = var.cloudfront_api_aliases
-  assets_aliases      = var.cloudfront_assets_aliases
-  acm_certificate_arn = var.cloudfront_acm_certificate_arn
-}
-
-module "waf" {
-  source = "../../modules/waf"
-
-  project_name = local.project_name
-  environment  = local.environment
-  tags         = local.common_tags
-
-  origin_verify_secret  = var.origin_verify_secret
-  api_gateway_stage_arn = module.apigateway.stage_arn
-}
-
-module "route53" {
-  count  = var.route53_hosted_zone_id != null ? 1 : 0
-  source = "../../modules/route53"
-
-  project_name = local.project_name
-  environment  = local.environment
-  tags         = local.common_tags
-
-  hosted_zone_id = var.route53_hosted_zone_id
-
-  portal_record_name = var.route53_portal_record_name
-  api_record_name    = var.route53_api_record_name
-  assets_record_name = var.route53_assets_record_name
-
-  portal_cloudfront_domain_name = module.cloudfront.portal_distribution_domain_name
-  api_cloudfront_domain_name    = module.cloudfront.api_distribution_domain_name
-  assets_cloudfront_domain_name = module.cloudfront.assets_distribution_domain_name
+  portal_aliases = var.cloudfront_portal_aliases
+  api_aliases    = var.cloudfront_api_aliases
+  assets_aliases = var.cloudfront_assets_aliases
+  # acm_certificate_arn = var.cloudfront_acm_certificate_arn
 }
 
 # -----------------------------------------------------------------------------
