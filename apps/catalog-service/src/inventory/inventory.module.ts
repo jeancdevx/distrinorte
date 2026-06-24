7d029c3fef584dfde2a8cabdd786179f7fe6b8eb
jeancdev <jcode2006@gmail.com>
2026-06-24 05:14:28 -0500

    refactor(catalog-service): remove InventoryStockClient and InventoryModule to simplify inventory management

--- apps/catalog-service/src/inventory/inventory.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-
-import { InventoryStockClient } from './inventory-stock.client.js'
-
-@Module({
-  providers: [InventoryStockClient],
-  exports: [InventoryStockClient]
-})
-export class InventoryModule {}
