519d18bc58d97987d9febb38f9c2c890b4a61c5c
jeancdev <jcode2006@gmail.com>
2026-06-24 05:14:08 -0500

    refactor(catalog-module): remove InventoryModule from imports to streamline dependencies

--- apps/catalog-service/src/catalog/catalog.module.ts
+++ apps/catalog-service/src/catalog/catalog.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common'
 
 import { DynamoDbModule } from '../dynamodb/dynamodb.module.js'
-import { InventoryModule } from '../inventory/inventory.module.js'
 import { RedisModule } from '../redis/redis.module.js'
 import { CatalogController } from './catalog.controller.js'
 import { CatalogService } from './catalog.service.js'
 
 @Module({
-  imports: [DynamoDbModule, RedisModule, InventoryModule],
+  imports: [DynamoDbModule, RedisModule],
   controllers: [CatalogController],
   providers: [CatalogService]
 })
