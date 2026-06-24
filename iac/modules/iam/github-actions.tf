8573150b862f2cda91780fd1ee727c4a4421b6b3
jeancdev <jcode2006@gmail.com>
2026-06-24 00:39:28 -0500

    feat(iam): extend GitHub OIDC policy to include environments in IAM configuration

--- iac/modules/iam/github-actions.tf
+++ iac/modules/iam/github-actions.tf
@@ -46,10 +46,16 @@ data "aws_iam_policy_document" "github_actions_assume" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values = [
-        for branch in var.github_oidc_branches :
-        "repo:${var.github_repository}:ref:refs/heads/${branch}"
-      ]
+      values = concat(
+        [
+          for branch in var.github_oidc_branches :
+          "repo:${var.github_repository}:ref:refs/heads/${branch}"
+        ],
+        [
+          for environment in var.github_oidc_environments :
+          "repo:${var.github_repository}:environment:${environment}"
+        ],
+      )
     }
   }
 }
