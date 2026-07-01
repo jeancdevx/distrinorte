fec4d88095fa4e29201ec672d00729a38cff65b1
jeancdev <jcode2006@gmail.com>
2026-07-01 15:50:52 -0500

    feat(dynamodb): add catalog availability table name to local configurations

--- iac/modules/dynamodb/locals.tf
+++ iac/modules/dynamodb/locals.tf
@@ -2,4 +2,5 @@ locals {
   name_prefix  = "${var.project_name}-${var.environment}"
   table_name   = "${local.name_prefix}-products"
   gsi_category = "category-index"
+  catalog_availability_table_name = "${local.name_prefix}-catalog-availability"
 }
