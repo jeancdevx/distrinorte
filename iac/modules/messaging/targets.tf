cf236af6dcca596160fcb650221747e2c38cccfb
jeancdev <jcode2006@gmail.com>
2026-07-08 01:00:26 -0500

    feat(iac): route stock transfer events to orders-events queue

--- iac/modules/messaging/targets.tf
+++ iac/modules/messaging/targets.tf
@@ -19,6 +19,14 @@ resource "aws_cloudwatch_event_target" "stock_rejected_orders_events" {
   arn            = aws_sqs_queue.orders_events.arn
 }
 
+
+resource "aws_cloudwatch_event_target" "orders_lifecycle_events_orders_events" {
+  rule           = aws_cloudwatch_event_rule.orders_lifecycle_events.name
+  event_bus_name = aws_cloudwatch_event_bus.main.name
+  target_id      = "orders-events"
+  arn            = aws_sqs_queue.orders_events.arn
+}
+
 resource "aws_cloudwatch_event_target" "projection_events_projections_work" {
   rule           = aws_cloudwatch_event_rule.projection_events.name
   event_bus_name = aws_cloudwatch_event_bus.main.name
