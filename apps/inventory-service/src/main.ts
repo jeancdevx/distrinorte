590b33527f1925237e3180d83075f5374c3f0467
jeancdev <jcode2006@gmail.com>
2026-07-08 04:31:06 -0500

    feat(inventory): configure PinoLogger and set service name in main.ts for improved logging

--- apps/inventory-service/src/main.ts
+++ apps/inventory-service/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core'
+import { Logger } from 'nestjs-pino'
 
 import { AppModule } from './app.module.js'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  process.env.SERVICE_NAME ??= 'inventory-service'
+
+  const app = await NestFactory.create(AppModule, { bufferLogs: true })
+  app.useLogger(app.get(Logger))
   app.setGlobalPrefix('inventory')
   await app.listen(process.env.PORT ?? 3002)
 }
