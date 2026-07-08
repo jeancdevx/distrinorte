08bc8f5816493514b4b37f9a69ab000c2b27d9b9
jeancdev <jcode2006@gmail.com>
2026-07-08 04:31:57 -0500

    feat(orders): configure PinoLogger and set service name in main.ts for improved logging

--- apps/orders-service/src/main.ts
+++ apps/orders-service/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core'
+import { Logger } from 'nestjs-pino'
 
 import { AppModule } from './app.module.js'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  process.env.SERVICE_NAME ??= 'orders-service'
+
+  const app = await NestFactory.create(AppModule, { bufferLogs: true })
+  app.useLogger(app.get(Logger))
   app.setGlobalPrefix('orders')
   await app.listen(process.env.PORT ?? 3001)
 }
